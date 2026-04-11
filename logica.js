99c6a5e96e358093d47e0625bece2671c0a02d7b
LUIZ HENRIQUE PEREIRA SOUZA <pereiraluiz5060@gmail.com>
2026-04-10 21:16:57 -0300

    2 dia de evolução

--- logica.js
+++ logica.js
@@ -5,6 +5,15 @@ async function buscarClima() {
     const resposta = await fetch(url);
     const dados = await resposta.json();
 
+    //Caso a Cidade não exista
+    if(dados.message === "NOT_FOUND"){
+        document.getElementById("resultado").innerHTML = `
+         <p>Essa cidade não existe</p>
+       `;
+       return;
+    }
+
+    // Caso de SUCESSO
     document.getElementById("resultado").innerHTML = `
      <h2>Clima em ${cidade}</h2>
      <p>Temperatura: ${dados.temperature}</p>
@@ -13,3 +22,10 @@ async function buscarClima() {
     `
 
 }
+
+document.getElementById("cidade").addEventListener("keypress",function(event) { 
+    if (event.key==="Enter") {
+        buscarClima();
+    }
+});
+
